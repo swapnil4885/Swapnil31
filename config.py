d534343781627f28a10d129806c6cd05a0c0bc9e
swapnil4885 <swapnilkumavat73@gmail.com>
2025-10-19 23:23:18 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+TELEGRAM_BOT_TOKEN = "8306875717:AAG34WyLvyi9qvCzQ4mppqUpu3TweHSTrO4"
+GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/17sooIHix_zMWDLi4YlUGOljXhMcE8Q78h9RnaGfXE7I/edit?usp=drivesdk"
+SERVICE_ACCOUNT_FILE = "service_account.json"
